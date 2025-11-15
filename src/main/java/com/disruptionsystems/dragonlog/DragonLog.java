ebee8f83f84537e12f79ad71d6d3c2d247083d91
disruption <gandhi@disruption-systems.com>
2025-11-15 11:46:04 +0100

    Make DragonLog More Universal by removing it's own configfile and providing parameters in the constructor of the DragonLog class instead

--- src/main/java/com/disruptionsystems/dragonlog/DragonLog.java
+++ src/main/java/com/disruptionsystems/dragonlog/DragonLog.java
@@ -12,50 +12,22 @@ import java.util.stream.Collectors;
 
 public class DragonLog {
 
-    private String logLocation;
-    private Properties props;
-    private String configLocation;
+    private final String logLocation;
     private File logFile;
+    private final LogLevel logLevel;
+    private final String logFileName;
 
-    public DragonLog(String configLocation){
-        this.configLocation = configLocation;
-        init();
+    public DragonLog(String logLocation, LogLevel level, String logFileName){
+        this.logLocation = logLocation;
+        this.logLevel = level;
+        this.logFileName = logFileName;
+        createBaseFileStructure();
     }
 
-
-    private void init(){
-        props = new Properties();
-        File configFile = new File(configLocation);
-        if (!configFile.exists()){
-            try {
-                createDefaultFileLayout(props, configFile);
-                try {
-                    props.load(new FileInputStream(configFile));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                logLocation = props.getProperty("logLocation");
-                this.createBaseFileStructure();
-            }
-            catch (IOException e){
-                System.out.println("Could not create or read file: "+configLocation+". Reason:\n" + e.getMessage());
-                System.exit(2);
-            }
-        }
-        try {
-            props.load(new FileInputStream(configFile));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        logLocation = props.getProperty("logLocation");
-        logFile = new File(logLocation + props.getProperty("logName"));
-        this.printToLog(LogLevel.INFORMATION, "System Startup complete. Awaiting Input");
-    }
     private void createBaseFileStructure(){
         File LogFolderFile = new File(logLocation);
-        if (!LogFolderFile.exists()){
-            LogFolderFile.mkdirs();
-        }
+        LogFolderFile.mkdirs();
+        logFile = new File(LogFolderFile, logFileName);
     }
     public String getLogLocation(){
         return logLocation;
@@ -65,37 +37,26 @@ public class DragonLog {
         return logFile.getName();
     }
 
-    public String getLogLevel(){
-        return (String) props.get("logLevel");
-    }
-
-    public static void createDefaultFileLayout(Properties props, File configFile) throws IOException {
-        props.setProperty("logLocation", "data/log/");
-        props.setProperty("logName", "log.chorus");
-        props.setProperty("logLevel", "INFORMATION");
-        props.store(new FileWriter(configFile), null);
-    }
-
-    public List<LogLevel> isBelowLevel(String level){
+    public List<LogLevel> isBelowLevel(LogLevel level){
         return switch (level) {
-            case "INFORMATION" ->
-                    new ArrayList<LogLevel>(List.of(new LogLevel[]{LogLevel.INFORMATION, LogLevel.WARNING, LogLevel.ERROR, LogLevel.CRITICAL, LogLevel.CRASH}));
-            case "WARNING" ->
-                    new ArrayList<LogLevel>(List.of(new LogLevel[]{LogLevel.WARNING, LogLevel.ERROR, LogLevel.CRITICAL, LogLevel.CRASH}));
-            case "ERROR" ->
-                    new ArrayList<LogLevel>(List.of(new LogLevel[]{LogLevel.ERROR, LogLevel.CRITICAL, LogLevel.CRASH}));
-            case "CRITICAL" ->
-                    new ArrayList<LogLevel>(List.of(new LogLevel[]{LogLevel.CRITICAL, LogLevel.CRASH}));
-            case "CRASH" ->
-                    new ArrayList<LogLevel>(List.of(new LogLevel[]{LogLevel.CRASH}));
+            case INFORMATION ->
+                    new ArrayList<>(List.of(new LogLevel[]{LogLevel.INFORMATION, LogLevel.WARNING, LogLevel.ERROR, LogLevel.CRITICAL, LogLevel.CRASH}));
+            case WARNING ->
+                    new ArrayList<>(List.of(new LogLevel[]{LogLevel.WARNING, LogLevel.ERROR, LogLevel.CRITICAL, LogLevel.CRASH}));
+            case ERROR ->
+                    new ArrayList<>(List.of(new LogLevel[]{LogLevel.ERROR, LogLevel.CRITICAL, LogLevel.CRASH}));
+            case CRITICAL ->
+                    new ArrayList<>(List.of(new LogLevel[]{LogLevel.CRITICAL, LogLevel.CRASH}));
+            case CRASH ->
+                    new ArrayList<>(List.of(new LogLevel[]{LogLevel.CRASH}));
             default -> null;
         };
     }
 
     public void printToLog(LogLevel level, String msg) {
-        if (isBelowLevel(getLogLevel()).contains(level)) {
+        if (isBelowLevel(logLevel).contains(level)) {
             logFile = new File(this.getLogFileName());
-            String message = getAppropriateEscapeChar(level) + "[" + LocalDate.now() + "    " + LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "]     \n" + getAppropriateEscapeChar(level) + "[" + level + "] " + msg + "\n";
+            String message = getAppropriateEscapeChar(level) + "[" + LocalDate.now() + "    " + LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "]\n" + getAppropriateEscapeChar(level) + "[" + level + "] " + msg + "\n";
             System.out.println(message);
             try {
                 if (!logFile.exists()) {
@@ -105,7 +66,7 @@ public class DragonLog {
                 writer.append(message);
                 writer.close();
             } catch (IOException e) {
-                System.out.println("\033[36mWARNING: CRITICAL: SYSTEM CANNOT PROCEED: COULD NOT CREATE FILE \"log.chorus\"");
+                System.out.println("\033[36mWARNING: CRITICAL: SYSTEM CANNOT PROCEED: COULD NOT CREATE FILE");
                 System.out.println(e.getMessage());
                 System.exit(1);
             }
--- src/main/test/main.java
+++ src/main/test/main.java
@@ -1,9 +1,11 @@
 import com.disruptionsystems.dragonlog.DragonLog;
 import com.disruptionsystems.dragonlog.LogLevel;
 
+import java.io.File;
+
 public class main {
     public static void main(String[] args) {
-        DragonLog logger = new DragonLog("data/config.chorus");
-        logger.printToLog(LogLevel.INFORMATION, "no");
+        DragonLog logger = new DragonLog("log", LogLevel.INFORMATION, "dragonlog.chorus");
+        logger.printToLog(LogLevel.WARNING, "no");
     }
 }
